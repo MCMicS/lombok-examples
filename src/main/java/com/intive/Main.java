ac0b757cea792fffe7524d2ace7773e441fb0403
Tomasz Krzysztof Zieleniewski <tomasz.zieleniewski@intive.com>
2018-08-22 11:56:08 +0200

    lombok-issue-1572: added main class with problem replication

---
+++ src/main/java/com/intive/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2018 intive.
+ *
+ * All rights reserved.
+ */
+package com.intive;
+
+public class Main {
+
+    private static final String URL = "http://intive.com";
+
+    public static void main(String[] args) {
+        var company = new Company();
+        company.setUrl(URL);
+    }
+}
